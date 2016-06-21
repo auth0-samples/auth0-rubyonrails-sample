b3570f1311bb3db5904cf86a916b8ab511c34080
iannyanes <iannyanes@gmail.com>
2016-06-20 22:38:14 -0500

    Folders Re-arrange
    According to the new template

--- 00-Starter-Seed/ruby-on-rails-api/app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  include Knock::Authenticable
-
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
-end
