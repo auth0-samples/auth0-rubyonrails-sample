ed8e9612476cba19f0ad754075ac62749cc483fa
Francisco Alvarisqueta <francisco.alvarisqueta@4hills.com>
2016-07-05 17:47:27 -0300

    Update credentials to use V1 credentials

--- 02-Custom-Login/config/routes.rb
+++ 02-Custom-Login/config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   get 'dashboard/show'
 
   get "/" => "home#show"
-  post "/" => "home#auth"
+  post "/" => "auth0#callback"
 
   get "/dashboard" => "dashboard#show"
 
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
+
+  get 'auth0/google_authorize' => 'auth0#google_authorize'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
