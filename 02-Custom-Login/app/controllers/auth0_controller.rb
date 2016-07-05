34d5be31e9f4282ea120c4adf42d6140c991aec2
Francisco Alvarisqueta <francisco.alvarisqueta@4hills.com>
2016-07-05 17:47:27 -0300

    Update credentials to use V1 credentials

--- 02-Custom-Login/app/controllers/auth0_controller.rb
+++ 02-Custom-Login/app/controllers/auth0_controller.rb
@@ -1,9 +1,59 @@
 class Auth0Controller < ApplicationController
   def callback
-    redirect_to '/dashboard'
+    begin
+      if params[:user]
+        if params[:signup]
+          signup
+        end
+        session[:token_id] = login
+      else
+        session[:token_id] = google_login
+      end
+      redirect_to '/dashboard'
+    rescue Auth0::Unauthorized
+      redirect_to '/', notice: 'Invalid email or password'
+    rescue => ex
+      redirect_to '/', notice: ex.message
+    end
+  end
+
+  def google_authorize
+    redirect_to client.authorization_url(
+      ENV['AUTH0_CALLBACK_URL'],
+      {
+        connection: 'google-oauth2',
+        scope:'openid'
+      }
+    ).to_s
   end
 
   def failure
     @error_msg = request.params['message']
   end
+
+  private
+
+  def google_login
+    client.obtain_user_tokens(params['code'], ENV['AUTH0_CALLBACK_URL'] , 'google-oauth2', 'openid')['id_token']
+  end
+
+  def login
+    client.login(
+      params[:user],
+      params[:password],
+      authParams: {
+       scope: 'openid name email'
+      },
+      connection:'Username-Password-Authentication'
+    )
+  end
+
+  def signup
+    client.signup(
+      params[:user],
+      params[:password]
+    )
+  end
+
+
 end
