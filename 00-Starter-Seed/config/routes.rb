4932e485f40dac36956283ee8d2f1e97365eb90a
Ignacio Jonas <ignaciojonas@gmail.com>
2016-07-19 11:59:30 -0300

    Rubocop fixes

--- 00-Starter-Seed/config/routes.rb
+++ 00-Starter-Seed/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  get "/" => "home#show"
+  get '/' => 'home#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
