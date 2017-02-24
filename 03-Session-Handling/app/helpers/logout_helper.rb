4e857e1a94bc48d94289d4c3dc94e0bd8c3b78f5
Leonardo Soubeste <leo.soubeste@eclinicalos.com>
2017-02-24 11:23:07 -0500

    Fix samples after SDK changes.

--- 03-Session-Handling/app/helpers/logout_helper.rb
+++ 03-Session-Handling/app/helpers/logout_helper.rb
@@ -3,8 +3,8 @@ module LogoutHelper
   def logout_url
     creds = { client_id: Rails.application.secrets.auth0_client_id,
               client_secret: Rails.application.secrets.auth0_client_secret,
-              api_version: 1,
-              domain: Rails.application.secrets.auth0_domain }
+              domain: Rails.application.secrets.auth0_domain,
+              token: Rails.application.secrets.auth0_token }
     client = Auth0Client.new(creds)
     client.logout_url(root_url)
   end
