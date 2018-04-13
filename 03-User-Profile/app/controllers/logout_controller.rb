9c8a375a42655992c5226fcd643b10d3dc32f2c4
Alexis Luque <aluque@indexlabs.com.uy>
2018-04-13 16:19:23 -0300

    Add logout to 03-User-Profile

---
+++ 03-User-Profile/app/controllers/logout_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+class LogoutController < ApplicationController
+  include LogoutHelper
+  def logout
+    reset_session
+    redirect_to logout_url.to_s
+  end
+end
--- 03-User-Profile/config/routes.rb
+++ 03-User-Profile/config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   get '/' => 'home#show'
 
   get '/dashboard' => 'dashboard#show'
+  get '/logout' => 'logout#logout'
 
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
+
+  root 'home#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
